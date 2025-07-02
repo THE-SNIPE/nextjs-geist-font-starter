905dc9be9653829ed363e02e6d716ba225688f2d
THE-SNIPE <sniperbmw6475@gmail.com>
2025-07-02 13:27:20 +0300

    Add vetjirani_app/lib/push_notification_service.dart 1751452039504

---
+++ vetjirani_app/lib/push_notification_service.dart
@@ -0,0 +1,40 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+class PushNotificationService {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging.instance;
+
+  Future<void> initialize() async {
+    // Request permission for iOS
+    NotificationSettings settings = await _firebaseMessaging.requestPermission(
+      alert: true,
+      badge: true,
+      sound: true,
+    );
+
+    if (settings.authorizationStatus == AuthorizationStatus.authorized) {
+      print('User granted permission for notifications');
+    } else {
+      print('User declined or has not accepted permission for notifications');
+    }
+
+    // Get the token for this device
+    String? token = await _firebaseMessaging.getToken();
+    print('FCM Token: $token');
+
+    // Handle foreground messages
+    FirebaseMessaging.onMessage.listen((RemoteMessage message) {
+      print('Received a message while in the foreground!');
+      print('Message data: ${message.data}');
+
+      if (message.notification != null) {
+        print('Message also contained a notification: ${message.notification}');
+      }
+    });
+
+    // Handle background and terminated state messages
+    FirebaseMessaging.onMessageOpenedApp.listen((RemoteMessage message) {
+      print('Message clicked!');
+      // TODO: Handle navigation based on message data
+    });
+  }
+}
